016bb742a46f70e2c7110ab1fc02665b27e31d54
sandip2224 <sandipan2224@gmail.com>
2021-12-05 11:48:36 +0530

    🔒️ Integrated cors and helmet for API security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const colors = require('colors')
-const path = require('path')
+const helmet=require('helmet')
+const cors=require('cors')
 require('dotenv').config({ path: './.env' })
 
 const connectDB = require('./api/config/db')
@@ -8,12 +9,14 @@ const connectDB = require('./api/config/db')
 const app = express()
 connectDB()
 
+app.use(cors())
+app.use(helmet())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/', require('./api/routes/urlRoute'))
 
-const server = app.listen(process.env.PORT || 3000, console.log(`Server running on port ${process.env.PORT || 3000}`.green.bold))
+const server = app.listen(process.env.PORT || 3000, console.log(`CORS-enabled server running on port ${process.env.PORT || 3000}`.green.bold))
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
